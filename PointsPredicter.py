5be01b08cf54e36206be5f6c73db9aa62a56e908
89JBWebb <89JBWebb@gmail.com>
2025-01-09 22:24:15 -0500

    added new test

---
+++ PointsPredicter.py
@@ -0,0 +1,24 @@
+#inputs
+#movement
+#toughness
+#save
+#involun
+#feel no pain
+#wounds
+#leadership
+#oc
+
+#ranged
+#range
+#attacks
+#BS
+#strength
+#ap
+#Damage
+
+#melee
+#attacks
+#BS
+#strength
+#ap
+#Damage
